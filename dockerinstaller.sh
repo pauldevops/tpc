6b17cefd9f062b439c3c20b12fe967c77e5f9744
pauldevops <31901404+pauldevops@users.noreply.github.com>
2020-06-23 18:43:20 +0300

    Update dockerinstaller.sh

--- dockerinstaller.sh
+++ dockerinstaller.sh
@@ -7,17 +7,19 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 echo "+------------------+"
 echo "|     CURL OK      |"
-echo "+------------------+
+echo "+------------------+"
 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 
+echo "======================================="
 echo "Docker added to source pcg successfully"
+echo "======================================="
 
 sudo apt update
 
 echo "+------------------+"
 echo "|    UPDATE OK     |"
-echo "+------------------+
+echo "+------------------+"
 
 sudo apt-cache policy docker-ce
 
@@ -27,13 +29,13 @@ read item
 case "$item" in
     y|Y) echo "Go-go-go"
         sudo apt install docker-ce -y
-		echo "+------------------+"
-		echo "| DOCKER INST OK   |"
-		echo "+------------------+
+	echo "+------------------+"
+	echo "| DOCKER INST OK   |"
+	echo "+------------------+"
     
-    sudo apt install py-pip python-dev libffi-dev openssl-dev gcc libc-dev
-    sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
+        sudo apt install py-pip python-dev libffi-dev openssl-dev gcc libc-dev
+        sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+        sudo chmod +x /usr/local/bin/docker-compose
     
 		sudo systemctl status docker
 		;;
